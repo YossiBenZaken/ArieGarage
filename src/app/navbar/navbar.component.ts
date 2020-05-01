2a561bd129c55cbb9757f63c8842d9c84f70eba4
Yossi Ben Zaken <yossibz@gmail.com>
2020-05-01 12:07:14 +0300

    v1

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+    const menuBtn = document.querySelector('.menu-btn');
+    let menuOpen = false;
+    menuBtn.addEventListener('click', () => {
+      if (!menuOpen) {
+        menuBtn.classList.add('open');
+      }
+      else {
+        menuBtn.classList.remove('open');
+      }
+      menuOpen = !menuOpen;
+    });
   }
-
 }
